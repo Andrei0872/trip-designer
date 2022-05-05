070581675d0bd9d7d4536a46e871045adf34cd8e
Andrei Gatej <andreigtj01@gmail.com>
2022-05-05 22:46:37 +0300

    feat(activities): display the list of activities based on selected categories

--- client/src/components/Activities.tsx
+++ client/src/components/Activities.tsx
@@ -1,46 +1,10 @@
-function Activities () {
-  const [categories, setCategories] = useState<string[] | null>(null);
-  const [activities, setActivities] = useState<string[] | null>(null);
-  const [selectedCategoriesMap, setSelectedCategoriesMap] = useState<SelectedCategories>({});
-
-  const selectedCategories = useMemo(() => categories?.filter(c => selectedCategoriesMap[c]), [selectedCategoriesMap]);
-
-  useEffect(() => {
-    fetchCategories().then(c => setCategories(c));
-  }, []);
-
-  useEffect(() => {
-    if (!selectedCategories) {
-      return;
-    }
-    
-    fetchActivitiesByCategories(selectedCategories).then(a => setActivities(a));
-  }, [selectedCategories]);
-
-  const onSelectedCategories = (selectedCategories: SelectedCategories) => {
-    setSelectedCategoriesMap(selectedCategories)
-  }
-
-  return (
-    <div className="activities">
-      {
-        categories
-          ? 
-          <Categories 
-            categories={categories} 
-            onSelectedCategories={onSelectedCategories}
-          />
-          : 'Loading categories...'
-      }
-
-    </div>
-  )
-}
-
-export default Activitiesimport './Activities.scss';
+import './Activities.scss';
 
 
+import { useEffect, useMemo, useRef } from "react";
 import { useState } from "react";
+import { fetchActivitiesByCategories, fetchCategories } from "../api/activities";
+import { Activity } from "../types/activity";
 import { TypeOfFirstArg } from "../types/utils";
 
 type OnSelectedCategories = (selectedCategories: { [k: string]: boolean }) => void;
@@ -76,3 +40,66 @@ const Categories: React.FC<{ categories: string[], onSelectedCategories: OnSelec
   )
 };
 
+const ActivitiesList: React.FC<{ activities: string[] | null }> = (props) => {
+  const { activities } = props;
+  
+  return (
+    <div className="activities-list">
+      <h2 className='activities-list__title'>Activities</h2>
+      
+      {
+        activities && activities.length
+          ? <ul className='activities-list__body'>
+              {
+                activities.map(
+                  a => <li className='activities-list__activity' key={a}>{a}</li>
+                )
+              }
+            </ul>
+          : 'No activities yet.'
+      }
+    </div>
+  )
+}
+
+function Activities () {
+  const [categories, setCategories] = useState<string[] | null>(null);
+  const [activities, setActivities] = useState<string[] | null>(null);
+  const [selectedCategoriesMap, setSelectedCategoriesMap] = useState<SelectedCategories>({});
+
+  const selectedCategories = useMemo(() => categories?.filter(c => selectedCategoriesMap[c]), [selectedCategoriesMap]);
+
+  useEffect(() => {
+    fetchCategories().then(c => setCategories(c));
+  }, []);
+
+  useEffect(() => {
+    if (!selectedCategories) {
+      return;
+    }
+    
+    fetchActivitiesByCategories(selectedCategories).then(a => setActivities(a));
+  }, [selectedCategories]);
+
+  const onSelectedCategories = (selectedCategories: SelectedCategories) => {
+    setSelectedCategoriesMap(selectedCategories)
+  }
+
+  return (
+    <div className="activities">
+      {
+        categories
+          ? 
+          <Categories 
+            categories={categories} 
+            onSelectedCategories={onSelectedCategories}
+          />
+          : 'Loading categories...'
+      }
+
+      { categories ? <ActivitiesList activities={activities} /> : null }
+    </div>
+  )
+}
+
+export default Activities
