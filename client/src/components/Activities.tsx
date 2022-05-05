89168d11b80042ef4c2e1f4349125f2a02baf79b
Andrei Gatej <andreigtj01@gmail.com>
2022-05-05 22:44:53 +0300

    feat(activities): add the _Categories_ subsection

--- client/src/components/Activities.tsx
+++ client/src/components/Activities.tsx
@@ -1,7 +1,78 @@
 function Activities () {
+  const [categories, setCategories] = useState<string[] | null>(null);
+  const [activities, setActivities] = useState<string[] | null>(null);
+  const [selectedCategoriesMap, setSelectedCategoriesMap] = useState<SelectedCategories>({});
+
+  const selectedCategories = useMemo(() => categories?.filter(c => selectedCategoriesMap[c]), [selectedCategoriesMap]);
+
+  useEffect(() => {
+    fetchCategories().then(c => setCategories(c));
+  }, []);
+
+  useEffect(() => {
+    if (!selectedCategories) {
+      return;
+    }
+    
+    fetchActivitiesByCategories(selectedCategories).then(a => setActivities(a));
+  }, [selectedCategories]);
+
+  const onSelectedCategories = (selectedCategories: SelectedCategories) => {
+    setSelectedCategoriesMap(selectedCategories)
+  }
+
   return (
-    <div>Activities</div>
+    <div className="activities">
+      {
+        categories
+          ? 
+          <Categories 
+            categories={categories} 
+            onSelectedCategories={onSelectedCategories}
+          />
+          : 'Loading categories...'
+      }
+
+    </div>
   )
 }
 
-export default Activities
+export default Activitiesimport './Activities.scss';
+
+
+import { useState } from "react";
+import { TypeOfFirstArg } from "../types/utils";
+
+type OnSelectedCategories = (selectedCategories: { [k: string]: boolean }) => void;
+type SelectedCategories = TypeOfFirstArg<OnSelectedCategories>;
+
+const Categories: React.FC<{ categories: string[], onSelectedCategories: OnSelectedCategories }> = (props) => {
+  const { categories, onSelectedCategories } = props;
+
+  const [selectedCategories, setSelectedCategories] = useState<{ [k: string]: boolean }>({});
+  
+  const selectCategory = (cat: string) => {
+    const newSelectedCategories = {
+      ...selectedCategories,
+      [cat]: !selectedCategories[cat]
+    };
+
+    setSelectedCategories(newSelectedCategories);
+    onSelectedCategories(newSelectedCategories);
+  }
+
+  return (
+    <ul className="categories">
+      {categories.map(c => (
+        <li
+          className={`categories__category ${selectedCategories[c] ? 'categories__category--selected' : ''}`}
+          onClick={() => selectCategory(c)}
+          key={c}
+        >
+          {c}
+        </li>
+      ))}
+    </ul>
+  )
+};
+
