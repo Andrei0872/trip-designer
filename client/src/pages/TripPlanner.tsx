f585a86585d23a211b16c1aec42bf899279237e0
Izotova Daria <izotovadaria0602@gmail.com>
2022-06-02 15:20:01 +0300

    refactor: make TripPlanner route component

--- client/src/pages/TripPlanner.tsx
+++ client/src/pages/TripPlanner.tsx
@@ -4,22 +4,26 @@ import Activities from "../components/Activities"
 import DailyPlanner from "../components/DailyPlanner"
 import Details from "../components/Details"
 import { ActivitiesProvider } from '../context/activities'
+import MainLayout from '../layout/MainLayout'
 
 function TripPlanner () {
   return (
-    <section className="trip-planner">
-      <div className="trip-planner__header">
-        <button className="trip-planner__save">Sign up to save</button>
-      </div>
+    <MainLayout>
+      <section className="trip-planner">
+        <div className="trip-planner__header">
+          <button className="trip-planner__save">Sign up to save</button>
+        </div>
 
-      <div className="trip-planner__body">
-        <Details />
-        <ActivitiesProvider>
-          <DailyPlanner />
-          <Activities />
-        </ActivitiesProvider>
-      </div>
-    </section>
+        <div className="trip-planner__body">
+          <Details />
+          <ActivitiesProvider>
+            <DailyPlanner />
+            <Activities />
+          </ActivitiesProvider>
+        </div>
+      </section>
+    </MainLayout>
+    
   )
 }
 
