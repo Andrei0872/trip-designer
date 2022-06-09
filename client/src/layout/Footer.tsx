102e19abb0b882031d3df353a6b60420d8a68cad
Ionescu-Ioan <ionescuionut3110@gmail.com>
2022-06-09 17:38:45 +0300

    first attempt to design footer

--- client/src/layout/Footer.tsx
+++ client/src/layout/Footer.tsx
@@ -1,7 +1,32 @@
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faMapPin } from '@fortawesome/free-solid-svg-icons';
+import { faCopyright } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './Footer.scss'
 function Footer () {
   return (
-    <footer>
-      <p>Some footer content</p>
+    <footer className="footer">
+      <div className='footer__info'>
+        <div className='footer__contact'>
+          <p><b>CONTACT:</b></p>
+          <p><FontAwesomeIcon fontSize={'.9rem'} icon={faEnvelope} /> Email: <a href="mailto:tripDesigner@gmail.com">tripDesigner@gmail.com</a></p>
+          <p><FontAwesomeIcon fontSize={'.9rem'} icon={faPhone} /> Phone: +40767309799</p>
+          <p><FontAwesomeIcon fontSize={'.9rem'} icon={faMapPin} /> Address: Str. Academiei nr.14, 010014 București</p>
+        </div>
+
+        <div className='footer__about'>
+          <p><b>ABOUT US:</b></p>
+          <p>We are a group of young programmers passionate about travel, <br/>who try to simplify the process of organizing the trip using technology.</p>
+        </div>
+      </div>
+      
+      <div className='footer__details'>
+        <p>Copyright <FontAwesomeIcon fontSize={'.9rem'} icon={faCopyright} /> 2022 TripDesigner </p>
+        <p>This website has learning purposes only!</p>
+        <p>Created on 15 April 2022</p>  
+      </div>
+        
     </footer>
   )
 }
