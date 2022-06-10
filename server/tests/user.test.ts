59a7d4dde468203afc3ff94aa8ce375398b70745
Ifrosă Diana-Maria <diana.ifrosa@gmail.com>
2022-06-10 19:17:31 +0300

    Deleted useless comments

--- server/tests/user.test.ts
+++ server/tests/user.test.ts
@@ -1,11 +1,9 @@
 require('dotenv').config()
-console.log(process.env.POSTGRES_USER)
 const userController = require('../controllers/user.controller').userController;
-const db = require('../db'); // create user 
+const db = require('../db');
 
 describe('login', () =>{
     test('empty email should result in unsuccessful login', async () => {
-        //FIX ME: connect to database, somehow it connects with "windows" username
         const result = await userController.loginUser("", "some_password");
         console.log(result.message);
         console.log(result);
