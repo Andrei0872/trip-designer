8468a89fcb074771635675b93538f040213f96d8
Ifrosă Diana-Maria <diana.ifrosa@gmail.com>
2022-06-10 19:14:22 +0300

    Added env variables

--- server/tests/user.test.ts
+++ server/tests/user.test.ts
@@ -1,13 +1,15 @@
+require('dotenv').config()
+console.log(process.env.POSTGRES_USER)
 const userController = require('../controllers/user.controller').userController;
 const db = require('../db'); // create user 
 
 describe('login', () =>{
-    test('empty email should result in unsuccessfull login', async () => {
+    test('empty email should result in unsuccessful login', async () => {
         //FIX ME: connect to database, somehow it connects with "windows" username
         const result = await userController.loginUser("", "some_password");
         console.log(result.message);
         console.log(result);
-        expect(result.message).toEqual("Invalid email");
+        expect(result.message).toEqual("Invalid email!");
     });
    
 });
