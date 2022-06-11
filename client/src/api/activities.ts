4086b941c2f65ecd56900867d5b0b73acae97b44
Andrei Gatej <andreigtj01@gmail.com>
2022-06-11 19:12:14 +0300

    refactor: migrate to axios

--- client/src/api/activities.ts
+++ client/src/api/activities.ts
@@ -1,18 +1,20 @@
 import { Activity, ActivityDTO } from "../types/activity";
 import { CategoryDTO } from "../types/category";
-
-const API_URL = process.env.REACT_APP_API_URL;
+import { axiosInstance } from "./axios";
 
 export const fetchCategories = (): Promise<string[]> => {
-  const URL = `${API_URL}/activities-categories`
-  return fetch(URL).then(r => r.json()).then((r: CategoryDTO) => r.data);
+  return axiosInstance.get('/activities-categories')
+    .then(r => r.data)
+    .then((r: CategoryDTO) => r.data);
 }
 
 export const fetchActivitiesByCategories = (categories?: string[]): Promise<Activity[]> => {
-  let URL = `${API_URL}/activities`;
+  let URL = `/activities`;
   if (!!categories) {
     URL += `?filter=${categories.join(',')}`;
   }
 
-  return fetch(URL).then(r => r.json()).then((r: ActivityDTO) => r.data);
+  return axiosInstance.get(URL)
+    .then(r => r.data)
+    .then((r: ActivityDTO) => r.data);
 }
