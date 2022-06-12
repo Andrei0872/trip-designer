8922e7f2f8ad79e75771bcf26818d44050b850b0
Andrei Gatej <andreigtj01@gmail.com>
2022-06-12 19:25:30 +0300

    feat(user): add login functionality

--- client/src/api/user.ts
+++ client/src/api/user.ts
@@ -7,3 +7,10 @@ export const registerUser = (formData: FormData): Promise<User> => {
     return axiosInstance.post('/user/register', body, { headers: { 'Content-Type': 'application/json' } })
         .then(r => r.data)
 }
+
+export const loginUser = (formData: FormData): Promise<User> => {
+    const body = JSON.stringify(Object.fromEntries(formData));
+
+    return axiosInstance.post('/user/login', body, { headers: { 'Content-Type': 'application/json' } })
+        .then(r => r.data)
+}
--- client/src/components/login-register/Login.tsx
+++ client/src/components/login-register/Login.tsx
@@ -1,15 +1,31 @@
+import { SyntheticEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../../api/user";
+import { useAxios } from "../../context/useAxios";
+import { useUserAuth } from "../../context/userAuthContext";
 import "./LoginRegister.scss"
+
 function Login() {
-    const handleSubmit = (e: { preventDefault: () => void; }) => {
+    const { setUser } = useUserAuth();
+    const navigate = useNavigate();
+
+    const handleSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
         e.preventDefault();
+
+        const formElement = e.target as HTMLFormElement;
+        loginUser(new FormData(formElement))
+            .then(u => {
+                setUser(u);
+                navigate("my-trips");
+            });
     }
 
     return (
         <form onSubmit={handleSubmit} className="logreg__form__login">
             <label className="logreg__form__login__label" htmlFor="login_email">Email</label>
-            <input className="logreg__form__login__input" type="text" id="login_email"/>
+            <input className="logreg__form__login__input" name="email" type="text" id="login_email"/>
             <label className="logreg__form__login__label" htmlFor="login_password">Password</label>
-            <input className="logreg__form__login__input" type="password" id="login_password"/>
+            <input className="logreg__form__login__input" name="password" type="password" id="login_password"/>
             <input className="logreg__form__login__button" type="submit" value="Continue"/>
         </form>
     )
