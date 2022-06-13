44929d795a1367e7d903465efcfc80cfc5aa5f18
Andrei Gatej <andreigtj01@gmail.com>
2022-06-14 01:30:05 +0300

    feat(Header): add *log out* button

--- client/src/layout/Header.tsx
+++ client/src/layout/Header.tsx
@@ -1,19 +1,35 @@
 import './Header.scss'
 
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useUserAuth } from '../context/userAuthContext'
 
 function Header () {
+  const { user, logOutUser } = useUserAuth();
+  const navigate = useNavigate();
+
+  const doesUserExist = !!user;
+
+  const doLogout = () => {
+    logOutUser();
+    navigate('/');
+  }
+
   return (
     <header className="header">
       <Link className='header__title' to='/'>TripDesigner</Link>
 
-      <nav className='header__nav'>
-        <ul className='header__tabs'>
-          <li className='header__tab'><Link to="/test">Create a trip</Link></li>
-          <li className='header__tab'><Link to="/my-trips">My trips</Link></li>
-          <li className='header__tab'><Link to="/settings">Settings</Link></li>
-        </ul>
-      </nav>
+      {
+        doesUserExist
+          ? <nav className='header__nav'>
+              <ul className='header__tabs'>
+                <li className='header__tab'><Link to="/test">Create a trip</Link></li>
+                <li className='header__tab'><Link to="/my-trips">My trips</Link></li>
+                <li className='header__tab'><Link to="/settings">Settings</Link></li>
+                <li className='header__tab' onClick={doLogout}><span>Log out</span></li>
+              </ul>
+            </nav>
+          : null
+      }
     </header>
   )
 }
