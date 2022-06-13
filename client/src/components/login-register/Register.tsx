150fc8cea18dc6db12055665e009c8c600775f15
Izotova Daria <izotovadaria0602@gmail.com>
2022-06-13 16:46:55 +0300

    feat: style the forms

--- client/src/components/login-register/Register.tsx
+++ client/src/components/login-register/Register.tsx
@@ -6,10 +6,13 @@ function Register() {
 
     return (
         <form onSubmit={handleSubmit} className="logreg__form__register">
-            <label className="logreg__form__register__label">Email<input type="text"/></label>
-            <label className="logreg__form__register__label">Password<input type="password"/></label>
-            <label className="logreg__form__register__label">Repeat password<input type="password"/></label>
-            <input type="submit" value="continue"/>
+            <label className="logreg__form__register__label" htmlFor="register_email">Email</label>
+            <input className="logreg__form__register__input" type="text" id="register_email"/>
+            <label className="logreg__form__register__label" htmlFor="register_password">Password</label>
+            <input className="logreg__form__register__input" type="password" id="register_password"/>
+            <label className="logreg__form__register__label" htmlFor="register_repeat_password">Repeat password</label>
+            <input className="logreg__form__register__input" type="password" id="register_repeat_password"/>
+            <input className="logreg__form__register__button" type="submit" value="Continue"/>
         </form>
     )
 }
