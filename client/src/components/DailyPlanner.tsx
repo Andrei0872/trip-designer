c91778591f7a488c3a35028c39421a140d14f153
Andrei Gatej <andreigtj01@gmail.com>
2022-06-14 15:20:26 +0300

    refactor(DailyPlanner): extract `DayActivity` into its `types` file

--- client/src/components/DailyPlanner.tsx
+++ client/src/components/DailyPlanner.tsx
@@ -2,6 +2,7 @@ import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { DragEventHandler, forwardRef, SyntheticEvent, useImperativeHandle, useMemo, useReducer, useState } from 'react';
 import { useActivities } from '../context/activities';
+import { DayActivity } from '../types/activity';
 import { ExportData } from '../types/utils';
 import './DailyPlanner.scss'
 
@@ -124,17 +125,6 @@ const DayActivities: React.FC<DayActivitiesProps> = (props) => {
     }
   </ul>;
 }
-
-interface DayActivity {
-  activityId: number;
-  activityName: string;
-  dayNumber: number;
-  hours: string;
-  note: string;
-  // Because the same activity might be added to the same day or to different days, 
-  // we need a way to distinguish the activities and we do that with help of the property below.
-  dayActivityId: string;
-};
 
 type DayActivitiesState = DayActivity[];
 
--- client/src/types/activity.ts
+++ client/src/types/activity.ts
@@ -11,3 +11,14 @@ export interface ActivityDTO {
   message: string;
   data: Activity[];
 }
+
+export interface DayActivity {
+  activityId: number;
+  activityName: string;
+  dayNumber: number;
+  hours: string;
+  note: string;
+  // Because the same activity might be added to the same day or to different days, 
+  // we need a way to distinguish the activities and we do that with help of the property below.
+  dayActivityId: string;
+};
